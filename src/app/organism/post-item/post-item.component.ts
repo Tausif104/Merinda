3296a4e3d4ee40307358d2c5bb57dce3c8a62161
tausif104 <tausifahmed49@gmail.com>
2023-01-30 03:12:08 +0600

    list of posts done

--- src/app/organism/post-item/post-item.component.ts
+++ src/app/organism/post-item/post-item.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PostItemComponent implements OnInit {
   @Input() img!: string;
+  @Input() cat!: string;
 
   constructor() {}
 
